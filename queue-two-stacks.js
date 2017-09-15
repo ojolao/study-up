a9def7b0f3316c1c593e44ea5a13cf6e7399775d
ojolao <ojolao@gmail.com>
2017-09-15 13:13:29 -0400

    define queueStack problem

---
+++ queue-two-stacks.js
@@ -0,0 +1,12 @@
+/* Implement a queue ↴ with 2 stacks. Your queue should have an enqueue and a
+dequeue function and it should be "first in first out" (FIFO). Optimize for
+the time cost of m function calls on your queue. These can be any mix of
+enqueue and dequeue calls. 
+
+OR 
+
+ * Write a stack using your preferred instantiation pattern. 
+ * Avoid using native array methods i.e., push, pop, and length.
+ * Once you're done, implement a queue using two stacks.
+
+*/
