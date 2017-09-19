104b0016c895b0bdfcf28c8415d26bdb566ff87f
ojolao <ojolao@gmail.com>
2017-09-19 13:44:29 -0400

    define problem

---
+++ delete-node-from-list.js
@@ -0,0 +1,19 @@
+/*
+Delete a node from a singly-linked list, ↴ given only a variable pointing to that node.
+Constraint: O(1) time and O(1) space
+The input could, for example, be the variable b below:
+
+  function LinkedListNode(value) {
+    this.value = value;
+    this.next = null;
+}
+
+var a = new LinkedListNode('A');
+var b = new LinkedListNode('B');
+var c = new LinkedListNode('C');
+
+a.next = b;
+b.next = c;
+
+deleteNode(b);
+*/
