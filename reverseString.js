be4c88c0c25f6c03fec2c81083ea3fbfc9301824
ojolao <ojolao@gmail.com>
2017-09-26 11:17:38 -0400

    define reverseString problem

---
+++ reverseString.js
@@ -0,0 +1,8 @@
+/*
+Write a function to reverse a string in-place.
+Since strings in JavaScript are immutable ↴ , first convert the string into an array of
+characters, do the in-place reversal on that array, and re-join that array into a string before
+returning it. This isn't technically "in-place" and the array of characters will cost O(n)
+additional space, but it's a reasonable way to stay within the spirit of the challenge. If you're
+comfortable coding in a language with mutable strings, that'd be even better!
+*/
