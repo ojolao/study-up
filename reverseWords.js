2aff3681db11cbb96300ab9f0adeb5ee5d03a32f
ojolao <ojolao@gmail.com>
2017-09-26 12:17:10 -0400

    implement reverseWords

--- reverseWords.js
+++ reverseWords.js
@@ -9,3 +9,17 @@ in-place
 When writing your function, assume the message contains only letters and spaces, and all words
 are separated by one space.
 */
+
+function reverseString(string) {
+  const wordArr = string.split(' ');
+  let start = 0;
+  let end = wordArr.length - 1;
+  while (start < end) {
+    const temp = wordArr[start];
+    wordArr[start] = wordArr[end];
+    wordArr[end] = temp;
+    start += 1;
+    end -= 1;
+  }
+  return wordArr.join(' ');
+}
