90557e83819cdbd5ecbdd1d1ce8ab8d0c668c2b5
ojolao <ojolao@gmail.com>
2017-09-26 12:46:22 -0400

    initial implementation of parenthesisMatch

--- parenthesisMatch.js
+++ parenthesisMatch.js
@@ -8,3 +8,26 @@ opening parenthesis, finds the corresponding closing parenthesis.
 Example: if the example string above is input with the number 10 (position of the first
 parenthesis), the output should be 79 (position of the last parenthesis).
 */
+
+function parenthesisMatch(string, parenIndex) {
+  // starting with parenIndex, confirm it's an open parenthetical, if it's not, throw an error.
+  const parenSoFar = ['('];
+  if (string[parenIndex] !== '(') {
+    throw new Error('that position is not a parenthesis');
+  } else {
+    // if it is, store in an array;
+    for (let i = parenIndex + 1; i < string.length; i++) {
+      if (string[i] === '(') {
+        parenSoFar.push(string[i]);
+      // find the next closing,
+      } else if (string[i] === ')') {
+        parenSoFar.pop();
+      // if after popping the array is empty, return the current index;
+        if (parenSoFar.length === 0) {
+          return i;
+        }
+      }
+    }
+    return 'that parenthesis does not close';
+  }
+}
