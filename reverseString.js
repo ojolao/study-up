221cd7455e4addaf78736825de185a8e41589d2b
ojolao <ojolao@gmail.com>
2017-09-26 11:20:54 -0400

    implement reverseString

--- reverseString.js
+++ reverseString.js
@@ -6,3 +6,17 @@ returning it. This isn't technically "in-place" and the array of characters will
 additional space, but it's a reasonable way to stay within the spirit of the challenge. If you're
 comfortable coding in a language with mutable strings, that'd be even better!
 */
+
+function reverseString(string) {
+  const stringArr = string.split('');
+  let start = 0;
+  let end = stringArr.length - 1;
+  while (start < end) {
+    const temp = stringArr[start];
+    stringArr[start] = stringArr[end];
+    stringArr[end] = temp;
+    start += 1;
+    end -= 1;
+  }
+  return stringArr.join('');
+}
