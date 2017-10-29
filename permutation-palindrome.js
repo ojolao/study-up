9a2ef10a7697cb28721f5b05674767482357e724
ojolao <ojolao@gmail.com>
2017-10-29 11:42:25 -0700

    implement permutationPalindrome

--- permutation-palindrome.js
+++ permutation-palindrome.js
@@ -7,4 +7,25 @@ Examples:
 "ivicc" should return true
 "civil" should return false
 "livci" should return false
+
+iterate through the string,
+make sure that each character (except for one) shows up twice
+use an object or maybe a set?--
+yes set, at the end, if the length is 0 or 1, return true, otherwise false
 */
+
+function permutationPalindrome(string) {
+  const chars = {};
+  for (let i = 0; i < string.length; i++) {
+    if (chars[string.charAt(i)]) {
+      delete chars[string.charAt(i)];
+    } else {
+      chars[string.charAt(i)] = true;
+    }
+  }
+  const remaining = Object.keys(chars);
+  if (remaining.length > 1) {
+    return false;
+  }
+  return true;
+}
