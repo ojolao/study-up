58cf4f53c6722ffac9dcf4279b18b10af2fb39d5
ojolao <ojolao@gmail.com>
2017-10-29 14:20:41 -0700

    implement initial solution

--- recursive-permutations.js
+++ recursive-permutations.js
@@ -7,3 +7,25 @@ To start, assume every character in the input string is unique.
 Your function can have loops—it just needs to also be recursive.
 
 */
+
+function recursivePermutations(string) {
+  // store set
+  const permutations = new Set();
+  const recursivePermutationsHelper = (permSoFar, remStr) => {
+    if (permSoFar.length === string.length) {
+      return permutations.add(permSoFar); // or return permSoFar
+    }
+    for (let i = 0; i < remStr.length; i++) {
+      const updatedPerm = permSoFar + remStr.charAt(i);
+      const updatedRem = remStr.split('');
+      updatedRem.splice(i, 1);
+      recursivePermutationsHelper(updatedPerm, updatedRem.join(''));
+    }
+  };
+  // need a helper function that takes permutation so far and what is left
+    // base case: if permSoFar.length === string.length, add to set and return
+    // iterate through what is left and call recursive case
+  recursivePermutationsHelper('', string);
+  // return set
+  return permutations;
+}
