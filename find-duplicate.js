9c95784723412474039d44ecb14a15a0482fd58d
ojolao <ojolao@gmail.com>
2017-10-29 21:17:35 -0700

    implement solution with constant space

--- find-duplicate.js
+++ find-duplicate.js
@@ -1,5 +1,17 @@
-/* I have an array of n + 1n+1 numbers. Every number in the range 1..n1..n appears once
+/* I have an array of n + 1 numbers. Every number in the range 1..n appears once
 except for one number that appears twice.
 Write a function for finding the number that appears twice.
 
 */
+
+function findDuplicate(array) {
+  for (let i = 0; i < array.length; i++) {
+    const index = array[i] - 1;
+    if (array[index] === false) {
+      return array[i];
+    } else {
+      array[index] = false;
+    }
+  }
+  return 'no duplicate';
+}
