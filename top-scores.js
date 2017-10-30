76e78890292d64e9268f34f6ff33a3c4577e4dea
ojolao <ojolao@gmail.com>
2017-10-29 20:34:11 -0700

    define problem

---
+++ top-scores.js
@@ -0,0 +1,12 @@
+/* You created a game that is more popular than Angry Birds.
+Each round, players receive a score between 0 and 100, which you use to rank them from
+highest to lowest. So far you're using an algorithm that sorts in O(n\lg{n})O(nlgn) time,
+but players are complaining that their rankings aren't updated fast enough. You need a faster
+sorting algorithm.
+
+Write a function that takes:
+
+an array of unsortedScores
+the highestPossibleScore in the game
+and returns a sorted array of scores in less than O(n\lg{n})O(nlgn) time.
+*/
